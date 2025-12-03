dbef9e3170d7bcf7d1cbc47ce1f8c599ebff5ca7
nadiad00 <ndalessio@birt.eus>
2025-12-04 00:34:22 +0100

    Header, footer, login y registro

--- camper/src/app/app.ts
+++ camper/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { Header } from "./shared/components/header/header";
+import { Footer } from "./shared/components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, Header, Footer],
   standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.scss'
--- camper/src/app/shared/components/header/header.ts
+++ camper/src/app/shared/components/header/header.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, HostListener, inject } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Auth } from '../../../core/services/auth';
 import { LanguageService, Language } from '../../../core/services/language';
@@ -7,13 +7,14 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, RouterLink, FormsModule],
+  imports: [CommonModule, NgIf, RouterLink, FormsModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
   authService = inject(Auth);
   languageService = inject(LanguageService);
+  menuOpen = false;
 
   get userName(): string {
     const user = this.authService.getUser();
@@ -26,5 +27,29 @@ export class Header {
 
   onLangChange(lang: Language) {
     this.languageService.changeLanguage(lang);
+  }
+
+  get isLogged(): boolean {
+    return !!this.authService.getUser();
+  }
+
+  toggleMenu(event: Event) {
+    event.stopPropagation();  // evita que el click se propague al documento
+    this.menuOpen = !this.menuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    if (!this.menuOpen) return;
+
+    const target = event.target as HTMLElement;
+
+    const insideMenu =
+      target.closest('.user-menu') ||
+      target.closest('.user-icon-wrapper');
+
+    if (!insideMenu) {
+      this.menuOpen = false;
+    }
   }
 }
